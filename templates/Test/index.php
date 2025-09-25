fe24c00d906cb93938b85c7affd266e3ed7f99ec
to19960425 <t.o19960425@gmail.com>
2025-09-25 13:08:44 +0900

    fix: ViteScriptsHelperのスクリプト呼び出しを修正し、Reactのリフレッシュランタイムを追加

--- templates/Test/index.php
+++ templates/Test/index.php
@@ -1,4 +1,4 @@
-<?= $this->ViteScripts->script('resources/js/main.tsx'); ?>
+<?= $this->ViteScripts->script('main'); ?>
 
 <!DOCTYPE html>
 <html lang="en">
@@ -10,7 +10,15 @@
 
 <body>
     <div class="el_btn">Count: 0</div>
+    <div id='app'></div>
 
+    <script type="module">
+        import RefreshRuntime from "http://localhost:3000/@react-refresh"
+        RefreshRuntime.injectIntoGlobalHook(window)
+        window.$RefreshReg$ = () => {}
+        window.$RefreshSig$ = () => (type) => type
+        window.__vite_plugin_react_preamble_installed__ = true
+    </script>
     <?= $this->fetch('script') ?>
 </body>
 
